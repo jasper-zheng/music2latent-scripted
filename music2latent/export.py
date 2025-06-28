ef5fdbebfc3ece22494c4f494df82e5cad62fc87
Jasper Zheng <jasperrr@Mac.mynet>
2025-06-28 14:40:26 +0100

    fixed the white noise in the forward method

--- music2latent/export.py
+++ music2latent/export.py
@@ -6,6 +6,20 @@ from .models import *
 
 from . import nn_diffusion_tilde
 
+from typing import Tuple, List
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ScriptedConv2d(nn.Conv2d):
+    """Custom Conv2d class with proper type annotations for TorchScript"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, x: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+        return self._conv_forward(x[0], self.weight, self.bias)
+
 def upsample_1d(x):
     return F.interpolate(x, scale_factor=2.0, mode="nearest")
 
@@ -22,47 +36,51 @@ class ScriptedUpsampleConv(nn.Module):
     def __init__(self, in_channels, out_channels=None, use_2d=False, normalize=False):
         super(ScriptedUpsampleConv, self).__init__()
         self.normalize = normalize
-
         self.use_2d = use_2d
-
         if out_channels is None:
             out_channels = in_channels
-        
-        if normalize:
-            self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels)
-        else:
-            self.norm = nn.Identity()
-
+        self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels) if normalize else nn.Identity()
         if use_2d:
             self.c = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding='same')
         else:
             self.c = nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=1, padding='same')
-
     def forward(self, x):
-
         x = self.norm(x)
-        
         if self.use_2d:
             x = upsample_2d(x)
         else:
             x = upsample_1d(x)
         x = self.c(x)
-
+        return x
+    
+class ScriptedUpsampleConvProj(nn.Module):
+    def __init__(self, in_channels, out_channels=None, use_2d=False, normalize=False):
+        super(ScriptedUpsampleConvProj, self).__init__()
+        self.normalize = normalize
+        self.use_2d = use_2d
+        if out_channels is None:
+            out_channels = in_channels
+        self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels) if normalize else nn.Identity()
+        if use_2d:
+            self.c = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding='same')
+        else:
+            self.c = nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=1, padding='same')
+    def forward(self, x: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+        x = self.norm(x[0])
+        if self.use_2d:
+            x = upsample_2d(x)
+        else:
+            x = upsample_1d(x)
+        x = self.c(x)
         return x
     
 class ScriptedDownsampleConv(nn.Module):
     def __init__(self, in_channels, out_channels=None, use_2d=False, normalize=False):
         super(ScriptedDownsampleConv, self).__init__()
         self.normalize = normalize
-
         if out_channels is None:
             out_channels = in_channels
-        
-        if normalize:
-            self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels)
-        else:
-            self.norm = nn.Identity()
-
+        self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels) if normalize else nn.Identity()
         if use_2d:
             self.c = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1)
         else:
@@ -72,20 +90,31 @@ class ScriptedDownsampleConv(nn.Module):
         x = self.norm(x)
         x = self.c(x)
         return x
+    
+class ScriptedDownsampleConvProj(nn.Module):
+    def __init__(self, in_channels, out_channels=None, use_2d=False, normalize=False):
+        super(ScriptedDownsampleConvProj, self).__init__()
+        self.normalize = normalize
+        if out_channels is None:
+            out_channels = in_channels
+        self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels) if normalize else nn.Identity()
+        if use_2d:
+            self.c = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1)
+        else:
+            self.c = nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+        x = self.norm(x[0])
+        x = self.c(x)
+        return x
 
 class ScriptedUpsampleFreqConv(nn.Module):
     def __init__(self, in_channels, out_channels=None, normalize=False):
         super(ScriptedUpsampleFreqConv, self).__init__()
         self.normalize = normalize
-
         if out_channels is None:
             out_channels = in_channels
-
-        if normalize:
-            self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels)
-        else:
-            self.norm = nn.Identity()
-
+        self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels) if normalize else nn.Identity()
         self.c = nn.Conv2d(in_channels, out_channels, kernel_size=(5,1), stride=1, padding='same')
 
     def forward(self, x):
@@ -94,23 +123,46 @@ class ScriptedUpsampleFreqConv(nn.Module):
         x = self.c(x)
         return x
 
+class ScriptedUpsampleFreqConvProj(nn.Module):
+    def __init__(self, in_channels, out_channels=None, normalize=False):
+        super(ScriptedUpsampleFreqConvProj, self).__init__()
+        self.normalize = normalize
+        if out_channels is None:
+            out_channels = in_channels
+        self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels) if normalize else nn.Identity()
+        self.c = nn.Conv2d(in_channels, out_channels, kernel_size=(5,1), stride=1, padding='same')
+
+    def forward(self, x: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+        x = self.norm(x[0])
+        x = F.interpolate(x, scale_factor=(4.0,1.0), mode="nearest")
+        x = self.c(x)
+        return x
+
 class ScriptedDownsampleFreqConv(nn.Module):
     def __init__(self, in_channels, out_channels=None, normalize=False):
         super(ScriptedDownsampleFreqConv, self).__init__()
         self.normalize = normalize
-
         if out_channels is None:
             out_channels = in_channels
-
-        if normalize:
-            self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels)
-        else:
-            self.norm = nn.Identity()
-
+        self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels) if normalize else nn.Identity()
         self.c = nn.Conv2d(in_channels, out_channels, kernel_size=(5,1), stride=(4,1), padding=(2,0))
 
     def forward(self, x):
         x = self.norm(x)
+        x = self.c(x)
+        return x
+    
+class ScriptedDownsampleFreqConvProj(nn.Module):
+    def __init__(self, in_channels, out_channels=None, normalize=False):
+        super(ScriptedDownsampleFreqConvProj, self).__init__()
+        self.normalize = normalize
+        if out_channels is None:
+            out_channels = in_channels
+        self.norm = nn.GroupNorm(min(in_channels//4, 32), in_channels) if normalize else nn.Identity()
+        self.c = nn.Conv2d(in_channels, out_channels, kernel_size=(5,1), stride=(4,1), padding=(2,0))
+
+    def forward(self, x: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+        x = self.norm(x[0])
         x = self.c(x)
         return x
     
@@ -170,8 +222,6 @@ class ScriptedResBlock(nn.Module):
         self.min_res_dropout = hparams.min_res_dropout
 
     def forward(self, x_in):
-    # def forward(self, x_in, time_emb=None):
-        
         x = self.norm1(x_in)
 
         if self.normalize_residual:
@@ -213,6 +263,98 @@ class ScriptedResBlock(nn.Module):
 
         return x
 
+class ScriptedResBlockProj(nn.Module):
+    def __init__(self, in_channels, out_channels, hparams, cond_channels=None, kernel_size=3, downsample=False, upsample=False, normalize=True, leaky=False, attention=False, heads=4, use_2d=False, normalize_residual=False):
+        super(ScriptedResBlockProj, self).__init__()
+        self.normalize = normalize
+        self.attention = attention
+        self.upsample = upsample
+        self.downsample = downsample
+        self.leaky = leaky
+        self.kernel_size = kernel_size
+        self.normalize_residual = normalize_residual
+        self.use_2d = use_2d
+        if use_2d:
+            Conv = nn.Conv2d
+        else:
+            Conv = nn.Conv1d
+        self.conv1 = Conv(in_channels, out_channels, kernel_size=kernel_size, stride=1, padding='same')
+        self.conv2 = zero_init(Conv(out_channels, out_channels, kernel_size=kernel_size, stride=1, padding='same'))
+        if in_channels!=out_channels:
+            self.res_conv = Conv(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        else:
+            self.res_conv = nn.Identity()
+        if normalize:
+            self.norm1 = nn.GroupNorm(min(in_channels//4, 32), in_channels)
+            self.norm2 = nn.GroupNorm(min(out_channels//4, 32), out_channels)
+        else:
+            self.norm1 = nn.Identity()
+            self.norm2 = nn.Identity()
+        if leaky:
+            self.activation = nn.LeakyReLU(negative_slope=0.2)
+        else:
+            self.activation = nn.SiLU()
+        if cond_channels is not None:
+            self.use_time_emb = True
+            self.proj_emb = zero_init(nn.Linear(cond_channels, out_channels))
+        else:
+            self.use_time_emb = False
+            self.proj_emb = nn.Identity()
+        self.dropout = nn.Dropout(hparams.dropout_rate)
+        if attention:
+            self.att = Attention(out_channels, heads, use_2d=use_2d)
+        else:
+            self.att = nn.Identity()
+
+        self.min_res_dropout = hparams.min_res_dropout
+
+    def forward(self, x_tup: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+    # def forward(self, x_in, time_emb):
+        
+        x_in = x_tup[0]
+        time_emb = x_tup[1]
+
+        x = self.norm1(x_in)
+
+        if self.normalize_residual:
+            y = x.clone()
+        else:
+            y = x_in.clone()
+
+        x = self.activation(x)
+        if self.downsample:
+            if self.use_2d:
+                x = downsample_2d(x)
+                y = downsample_2d(y)
+            else:
+                x = downsample_1d(x)
+                y = downsample_1d(y)
+        if self.upsample:
+            if self.use_2d:
+                x = upsample_2d(x)
+                y = upsample_2d(y)
+            else:
+                x = upsample_1d(x)
+                y = upsample_1d(y)
+        x = self.conv1(x)
+        # if time_emb is not None:
+        if self.use_2d:
+            x = x+self.proj_emb(time_emb)[:,:,None,None]
+        else:
+            x = x+self.proj_emb(time_emb)[:,:,None]
+        if self.normalize:
+            x = self.norm2(x)
+        x = self.activation(x)
+        if x.shape[-1]<=self.min_res_dropout:
+            x = self.dropout(x)
+        x = self.conv2(x)
+        y = self.res_conv(y)
+        x = x+y
+        
+        x = self.att(x)
+
+        return x
+
 class ScriptedEncoder(nn.Module):
     def __init__(self, hparams):
         super(ScriptedEncoder, self).__init__()
@@ -233,7 +375,6 @@ class ScriptedEncoder(nn.Module):
         
         # DOWNSAMPLING
         down_layers = []
-        down_res_layers = []
         for i, (num_layers,multiplier) in enumerate(zip(layers_list,hparams.multipliers_list)):
             output_channels = hparams.base_channels*multiplier
             for num in range(num_layers):
@@ -373,6 +514,8 @@ class ScriptedDecoder(nn.Module):
 
 
 class ScriptedUNet(nn_diffusion_tilde.Module):
+    __constants__ = ['mods']
+
     def __init__(self, hparams, sigma_rescale):
         super(ScriptedUNet, self).__init__()
         
@@ -402,15 +545,15 @@ class ScriptedUNet(nn_diffusion_tilde.Module):
         for i, (num_layers,multiplier) in enumerate(zip(hparams.layers_list,hparams.multipliers_list)):
             output_channels = hparams.base_channels*multiplier
             for num in range(num_layers):
-                down_layers.append(Conv(output_channels, output_channels, kernel_size=1, stride=1, padding=0))
-                down_layers.append(ScriptedResBlock(output_channels, output_channels, hparams, cond_channels = hparams.cond_channels, normalize=hparams.normalization, attention=hparams.attention_list[i]==1, heads=hparams.heads, use_2d=True))                
+                down_layers.append(ScriptedConv2d(output_channels, output_channels, kernel_size=1, stride=1, padding=0))
+                down_layers.append(ScriptedResBlockProj(output_channels, output_channels, hparams, cond_channels = hparams.cond_channels, normalize=hparams.normalization, attention=hparams.attention_list[i]==1, heads=hparams.heads, use_2d=True))                
                 input_channels = output_channels
             if i!=(len(hparams.layers_list)-1):
                 output_channels = hparams.base_channels*hparams.multipliers_list[i+1]
                 if hparams.freq_downsample_list[i]==1:
-                    down_layers.append(ScriptedDownsampleFreqConv(input_channels, output_channels))
+                    down_layers.append(ScriptedDownsampleFreqConvProj(input_channels, output_channels))
                 else:
-                    down_layers.append(ScriptedDownsampleConv(input_channels, output_channels, use_2d=True))
+                    down_layers.append(ScriptedDownsampleConvProj(input_channels, output_channels, use_2d=True))
 
         # UPSAMPLING
         multipliers_list_upsampling = list(reversed(hparams.multipliers_list))[1:]+list(reversed(hparams.multipliers_list))[:1]
@@ -418,14 +561,14 @@ class ScriptedUNet(nn_diffusion_tilde.Module):
         up_layers = []      
         for i, (num_layers,multiplier) in enumerate(zip(reversed(hparams.layers_list),multipliers_list_upsampling)):
             for num in range(num_layers):
-                up_layers.append(Conv(input_channels, input_channels, kernel_size=1, stride=1, padding=0))
-                up_layers.append(ScriptedResBlock(input_channels, input_channels, hparams, cond_channels = hparams.cond_channels, normalize=hparams.normalization, attention=list(reversed(hparams.attention_list))[i]==1, heads=hparams.heads, use_2d=True))
+                up_layers.append(ScriptedConv2d(input_channels, input_channels, kernel_size=1, stride=1, padding=0))
+                up_layers.append(ScriptedResBlockProj(input_channels, input_channels, hparams, cond_channels = hparams.cond_channels, normalize=hparams.normalization, attention=list(reversed(hparams.attention_list))[i]==1, heads=hparams.heads, use_2d=True))
             if i!=(len(hparams.layers_list)-1):
                 output_channels = hparams.base_channels*multiplier
                 if freq_upsample_list[i]==1:
-                    up_layers.append(ScriptedUpsampleFreqConv(input_channels, output_channels))
+                    up_layers.append(ScriptedUpsampleFreqConvProj(input_channels, output_channels))
                 else:
-                    up_layers.append(ScriptedUpsampleConv(input_channels, output_channels, use_2d=True))
+                    up_layers.append(ScriptedUpsampleConvProj(input_channels, output_channels, use_2d=True))
                 input_channels = output_channels
                 
         self.conv_decoded = Conv(input_channels, input_channels, kernel_size=1, stride=1, padding=0)
@@ -508,30 +651,30 @@ class ScriptedUNet(nn_diffusion_tilde.Module):
         l = 0
         for i, layer in enumerate(self.down_layers):
             l = l + 1
-            # if l == 1 or l == 3:
             if l % self.layers_list[k] == 1 and l != self.layers_list[k]*2+1:
-                d = layer(pyramid_latents[k])
-                x = (x+d)/1.4142135 # np.sqrt(2.)
+                d = layer((pyramid_latents[k],time_emb))
+                x = (x+d)/self.sqrt2
             elif l % self.layers_list[k] == 0 and l != self.layers_list[k]*2+1:
-                x = layer(x)
+                x = layer((x, time_emb))
                 skip_list.append(x)
             elif l == self.layers_list[k]*2+1 and i != (len(self.down_layers)-1):
-                x = layer(x)
+                x = layer((x,time_emb))
                 k = k + 1
                 l = 0
-            
+        # x, skip_list = self.down_layers(x, time_emb, pyramid_latents, self.layers_list)
+
         # UPSAMPLING
         k = 0
         l = 0
         for i, layer in enumerate(self.up_layers):
             l = l + 1
             if l % self.layers_list[k] == 1 and l != self.layers_list[k]*2+1:
-                d = layer(pyramid_latents[-k-1])
-                x = (x+skip_list.pop()+d)/1.732051 # np.sqrt(3.)
+                d = layer((pyramid_latents[-k-1],time_emb))
+                x = (x+skip_list.pop()+d)/self.sqrt3
             elif l % self.layers_list[k] == 0 and l != self.layers_list[k]*2+1:
-                x = layer(x)
+                x = layer((x, time_emb)) 
             elif l == self.layers_list[k]*2+1 and i != (len(self.up_layers)-1):
-                x = layer(x)
+                x = layer((x,time_emb))
                 k = k + 1
                 l = 0
                 
